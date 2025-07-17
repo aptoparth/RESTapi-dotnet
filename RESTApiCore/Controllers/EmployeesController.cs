7ca395808dd664b60e49e64092229afef314d5b6
Parth Makwana <pmakwana@aptologics.com>
2025-07-17 12:50:01 +0530

    add framework of webapi and entity framework

---
+++ RESTApiCore/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTApiCore.Controllers
+{
+    // localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        
+    }
+}
